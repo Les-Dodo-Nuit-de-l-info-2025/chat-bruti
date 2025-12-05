f43dc246ff4e550bf1c996f0848f9dd1c0210e44
TheoNicod <theonicod@hotmail.fr>
2025-12-05 05:55:36 +0100

    a

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@
 
   export default defineConfig({
     plugins: [react()],
-    base: '/chat-bruti/',
+    base: './',
     resolve: {
       extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
       alias: {
